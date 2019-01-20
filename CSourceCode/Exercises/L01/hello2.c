04730971ff3f3fa1944d4d58d30c660dce9208f3
joseph frampton <josephjamesframpton@gmail.com>
2019-01-19 20:44:45 -0700

    init commit
    JJF

---
+++ CSourceCode/Exercises/L01/hello2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  /* "int argc" is the number of arguments passed to main*/
+  /* "char **argv" or "char *argv[]" is an array of the arguments passed */
+  printf("hello world\n");
+  return 0;
+}
